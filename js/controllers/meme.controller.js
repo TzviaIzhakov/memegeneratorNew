e143e0086ca4211d0d83e1bd2dac0e196c4ec589
TzviaIzhakov <tzvia.izhakov@gmail.com>
2023-08-23 20:55:50 +0300

    setting a frame on each line-need bo fixede

--- js/controllers/meme.controller.js
+++ js/controllers/meme.controller.js
@@ -2,7 +2,8 @@
 
 let gElCanvas;
 let gCtx;
-
+let gCountChar = 0;
+let gContinuedline;
 function onInit() {
   console.log('Hi');
   gElCanvas = document.querySelector('canvas');
@@ -13,8 +14,6 @@ function renderMeme() {
   const elImg = new Image();
   const meme = getMeme();
   const urlMeme = getMemeUrl();
-  console.log(urlMeme);
-  console.log(elImg);
   elImg.src = `${urlMeme}`;
 
   elImg.onload = () => {
@@ -38,23 +37,164 @@ function changeFont(dxSize) {
 }
 
 function drawText(elInput) {
-  setLineTxt(elInput.value);
+  gCountChar++;
+  let mesureWidthInput = gCtx.measureText(elInput.value).width;
+  if (mesureWidthInput + 100 >= gElCanvas.width) {
+    setLineTxt(`${elInput.value}\n`);
+    // gContinuedline = elInput.value.substring(gCountChar);
+    // console.log(gContinuedline, 'gContinuedline');
+  } else {
+    setLineTxt(elInput.value);
+  }
   renderMeme();
 }
+// function drawText(elInput) {
+//   const maxTextWidth = gElCanvas.width - 100; // Adjust the threshold as needed
+//   const text = elInput.value;
+//   const words = text.split(' ');
+//   let wrappedWords = [];
+
+//   // Calculate which words exceed the canvas width
+//   for (let i = 0; i < words.length; i++) {
+//     const lineText = wrappedWords.join(' ');
+//     const testLine = lineText + (lineText ? ' ' : '') + words[i];
+//     const testWidth = gCtx.measureText(testLine).width;
+
+//     if (testWidth > maxTextWidth) {
+//       wrappedWords.push('\n', words[i]);
+//     } else {
+//       wrappedWords.push(words[i]);
+//     }
+//   }
+
+//   // Update the line text with wrapped words
+//   setLineTxt(wrappedWords.join(' '));
+
+//   renderMeme();
+// }
 
 function downloadCanvas(elLink) {
   const dataUrl = gElCanvas.toDataURL();
   console.log('dataUrl', dataUrl);
 
   elLink.href = dataUrl;
-  // Set a name for the downloaded file
   elLink.download = 'my-meme';
 }
+
+// function renderLines() {
+//   const meme = getMeme();
+//   const elInput = document.querySelector('.txt');
+//   console.log(gCtx.measureText(elInput.value).width);
+//   meme.lines.forEach((line, i) => {
+//     gCtx.fillStyle = line.color;
+//     gCtx.font = `${line.size}px Arial`;
+//     if (i == 1) {
+//       setCoords(i, 100, gElCanvas.height - 100);
+//     }
+//     if (i >= 2) {
+//       setCoords(i, 100, gElCanvas.height / 2 - 5);
+//     }
+
+//     if (i === meme.selectedLineIdx) {
+//       gCtx.strokeStyle = 'blue'; // Set the frame color
+//       gCtx.lineWidth = 2;
+//       let mesureWidthInput = gCtx.measureText(elInput.value).width;
+//       let size = line.size;
+
+//       const hightFrame =
+//         mesureWidthInput + 100 >= gElCanvas.width ? (size += 100) : size + 5;
+//       console.log(hightFrame);
+//       gCtx.strokeRect(
+//         line.x - 5,
+//         line.y - line.size,
+//         mesureWidthInput,
+//         hightFrame
+//       );
+//       console.log(line.txt);
+//       gCtx.fillText(line.txt, line.x, line.y);
+//       // drawWrappedText(gCtx, line.txt, line.x, line.y, gElCanvas.width);
+//     }
+//   });
+// }
 function renderLines() {
   const meme = getMeme();
-  meme.lines.forEach((line) => {
+  const elInput = document.querySelector('.txt');
+
+  meme.lines.forEach((line, i) => {
     gCtx.fillStyle = line.color;
     gCtx.font = `${line.size}px Arial`;
-    gCtx.fillText(line.txt, 100, 100);
+
+    if (i == 1) {
+      setCoords(i, 100, gElCanvas.height - 100);
+    }
+    if (i >= 2) {
+      setCoords(i, 100, gElCanvas.height / 2 - 5);
+    }
+    // gCtx.fillText(line.txt, line.x, line.y);
+
+    if (i === meme.selectedLineIdx) {
+      gCtx.strokeStyle = 'blue'; // Set the frame color
+      gCtx.lineWidth = 2;
+
+      const textWidth = gCtx.measureText(line.txt).width;
+      const hightFrame =
+        textWidth + 100 >= gElCanvas.width ? line.size + 100 : line.size + 5;
+
+      gCtx.strokeRect(line.x - 5, line.y - line.size, textWidth, hightFrame);
+    }
+
+    // Draw the text, handling wrapping if needed
+    const words = line.txt.split(' ');
+    let lineText = '';
+    let y = line.y;
+
+    for (let word of words) {
+      const testLine = lineText + word + ' ';
+      const testWidth = gCtx.measureText(testLine).width;
+
+      if (testWidth > gElCanvas.width - 100) {
+        gCtx.fillText(lineText, line.x, y);
+        lineText = word + ' ';
+        y += line.size;
+      } else {
+        lineText = testLine;
+      }
+    }
+
+    gCtx.fillText(lineText, line.x, y);
   });
+}
+
+function drawWrappedTextWithBorder(context, text, x, y, maxWidth) {
+  const words = text.split(' ');
+  let line = '';
+
+  for (let i = 0; i < words.length; i++) {
+    const testLine = line + words[i] + ' ';
+    const testWidth = context.measureText(testLine).width;
+
+    if (testWidth > maxWidth && i > 0) {
+      context.strokeText(line, x, y);
+      context.fillText(line, x, y);
+      line = words[i] + ' ';
+      y += fontSize; // Move to the next line
+    } else {
+      line = testLine;
+    }
+  }
+
+  context.strokeText(line, x, y);
+  context.fillText(line, x, y);
+}
+
+function onAddLine() {
+  const elInput = document.querySelector('.txt');
+  addLine(elInput.value, 'black');
+  drawText(elInput);
+  renderMeme();
+}
+
+function onSwitchLine() {
+  switchLine();
+  renderMeme();
 }
--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -8,60 +8,89 @@ let gMeme = {
   selectedImgId: 1,
   selectedLineIdx: 0,
   lines: [
-    {
-      txt: 'I sometimes eat Falafel',
-      size: 50,
-      color: 'pink',
-    },
-    {
-      txt: 'I love to eat Hamburger',
-      size: 40,
-      color: 'red',
-    },
+    _createLine('I sometimes eat Falafel', 'black'),
+    _createLine('I love to eat Hamburger', 'red'),
   ],
 };
 
+var selectedLineIdx = gMeme.selectedLineIdx;
 function getMeme() {
   return gMeme;
 }
 
+function addLine(txt, color) {
+  const newLine = _createLine(txt, color);
+  gMeme.lines.push(newLine);
+  gMeme.selectedLineIdx = gMeme.lines.length - 1;
+  selectedLineIdx = gMeme.selectedLineIdx;
+  console.log(selectedLineIdx);
+  // _saveBooks();
+}
+
+function _createLine(txt, color = 'black', x = 100, y = 100) {
+  return {
+    txt,
+    size: 30,
+    color,
+    x,
+    y,
+  };
+}
+
+function switchLine() {
+  if (selectedLineIdx + 1 === gMeme.lines.length) {
+    selectedLineIdx = 0;
+    gMeme.selectedLineIdx = selectedLineIdx;
+    console.log('selectedLineIdx', gMeme.selectedLineIdx);
+    return;
+  }
+
+  selectedLineIdx++;
+  gMeme.selectedLineIdx = selectedLineIdx;
+  console.log('selectedLineIdx', gMeme.selectedLineIdx);
+}
+
 function getMemeUrl() {
   const memeUrl = gImgs.find((img) => img.id === gMeme.selectedImgId);
-  console.log(memeUrl.url);
   return memeUrl.url;
 }
 
 function getLineTxt() {
-  return gMeme.lines[0].txt;
+  return gMeme.lines[selectedLineIdx].txt;
 }
 
 function setLineTxt(newLine) {
-  gMeme.lines[0].txt = newLine;
+  gMeme.lines[selectedLineIdx].txt = newLine;
+  console.log(newLine, 'newLine');
 }
 
 function getColorMeme() {
-  return gMeme.lines[0].color;
+  return gMeme.lines[selectedLineIdx].color;
 }
 
 function setColor(color) {
-  gMeme.lines[0].color = color;
+  gMeme.lines[selectedLineIdx].color = color;
 }
 
 function getFontSize() {
-  return gMeme.lines[0].size;
+  return gMeme.lines[selectedLineIdx].size;
 }
 
 function setFontSize(fontSize) {
-  let sizeMeme = gMeme.lines[0].size;
+  let sizeMeme = gMeme.lines[selectedLineIdx].size;
   console.log(sizeMeme, 'before');
   if (fontSize === -10 && sizeMeme === 10) {
     return;
   }
-  gMeme.lines[0].size += fontSize;
+  gMeme.lines[selectedLineIdx].size += fontSize;
   console.log(sizeMeme, 'after');
 }
 
 function setImg(id) {
   gMeme.selectedImgId = id;
-  console.log(gMeme.selectedImgId);
+}
+
+function setCoords(idx, x, y) {
+  gMeme.lines[idx].x = x;
+  gMeme.lines[idx].y = y;
 }
